5395dce432855a6d7d3fd70b2f4e986297977c03
sudhanshukumar722 <129382440+sudhanshukumar722@users.noreply.github.com>
2023-07-18 13:58:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import ButtonFilter from "./components/ButtonFilter";
+import Jumbotron from "./components/Jumbotron";
+import ListApi from "./components/ListApi";
+import useAxios from "./hooks/useAxios";
+
+function App() {
+  const { fetchData, response, loading } = useAxios('entries');
+
+  useEffect(() => {
+    fetchData({ params: { category: 'Animals' } });
+  }, []);
+
+  return (
+    <div>
+      <Jumbotron fetchData={fetchData} />
+      <div className="container mx-auto max-w-4xl">
+        <ButtonFilter fetchData={fetchData} />
+        <ListApi response={response} loading={loading} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
